7fca77fdc1aedc36a2f977cb8d7469559b3e4b8f
mathias77515 <mathias.p.regnier@gmail.com>
2024-04-10 18:29:54 +0200

    add try except

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import yaml
 from pipeline import *
 from pyoperators import *
 
+import sys
+
+try:
+    file = str(sys.argv[1])
+except IndexError:
+    file = 1
+    
 if __name__ == "__main__":
 
     ### Common MPI arguments
