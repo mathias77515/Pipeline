9a208c3b65edccdf9ba38347de5dd5f9a09542b0
mathias77515 <mathias.p.regnier@gmail.com>
2024-04-10 20:13:11 +0200

    update cg.py

--- main.sh
+++ main.sh
@@ -4,15 +4,16 @@
 
 # we ask for n MPI tasks with N cores each on c nodes
 
-#SBATCH --partition=bigmem
+#SBATCH --partition=htc
 #SBATCH --nodes=1                # c
-#SBATCH --ntasks-per-node=1      # n
-#SBATCH --cpus-per-task=6        # N
-#SBATCH --mem=6G
+#SBATCH --ntasks-per-node=2      # n
+#SBATCH --cpus-per-task=2        # N
+#SBATCH --mem=10G
 #SBATCH --time=0-10:00:00
 #SBATCH --output=mulitple_jobs_%j.log
+#SBATCH --array=1-10
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
-#mpirun -np $SLURM_NTASKS python main.py
-python main.py
+mpirun -np $SLURM_NTASKS python main.py
+#python main.py
