Correct issue in the formula to compute the resolution when we do not add convolution during reconstruction and modify the implementation in a clearer manner

pipeline.py
@@ -2,6 +2,7 @@ import numpy as np
 import yaml
 import pickle
 import time
+import scipy
 
 from model.models import *
 from plots.plotter import *
@@ -283,6 +284,23 @@ class PipelineFrequencyMapMaking:
 
     
         return d, dmono
+    def _get_scalar_acquisition_operator(self):
+        """
+        Function that will compute "scalar acquisition operatord" by applying the acquisition operators to a vector full of ones.
+        These scalar operators will be used to compute the resolutions in the case where we do not add convolutions during reconstruction.
+        """
+        ### Import the acquisition operators
+        acquisition_operators = self.joint.qubic.H
+        
+        ### Create the vector full of ones which will be used to compute the scalar operators
+        vector_ones = np.ones(acquisition_operators[0].shapein)
+
+        ### Apply each sub_operator on the vector
+        scalar_acquisition_operators = np.empty(len(self.joint.qubic.allnus))
+        for freq in range(len(self.joint.qubic.allnus)):
+            scalar_acquisition_operators[freq] = np.mean(acquisition_operators[freq](vector_ones))
+        return scalar_acquisition_operators
+    
     def _get_convolution(self):
         """
 
@@ -309,10 +327,41 @@ class PipelineFrequencyMapMaking:
                 self.fwhm_rec = np.append(self.fwhm_rec, np.min(self.joint.qubic.allfwhm[irec*self.fsub_out:(irec+1)*self.fsub_out]))
    
         elif self.params['QUBIC']['convolution_in'] and self.params['QUBIC']['convolution_out'] is False:
+            ### Compute expected resolutions and frequencies when not adding convolutions during reconstruction
+            ### See FMM annexe B to understand the computations
             self.fwhm_rec = np.array([])
-            for irec in range(self.params['QUBIC']['nrec']):
-                self.fwhm_rec = np.append(self.fwhm_rec, np.mean(self.joint.qubic.allfwhm[irec*self.fsub_out:(irec+1)*self.fsub_out]))
 
+            scalar_acquisition_operators = self._get_scalar_acquisition_operator()
+            f_dust = c.ModifiedBlackBody(nu0=353, beta_d=1.54)
+            
+            for irec in range(self.params['QUBIC']['nrec']):
+                numerator_fwhm , denominator_fwhm = 0, 0
+                numerator_nus, denominator_nus = 0, 0
+                for jsub in range(irec*self.fsub_out, (irec+1)*self.fsub_out):
+                    # Compute the expected reconstructed resolution for sub-acquisition
+                    numerator_fwhm += scalar_acquisition_operators[jsub] * f_dust.eval(self.joint.qubic.allnus[jsub]) * self.fwhm_in[jsub]
+                    denominator_fwhm += scalar_acquisition_operators[jsub] * f_dust.eval(self.joint.qubic.allnus[jsub])
+
+                    # Compute the expected reconstructed frequencies for sub_acquisition
+                    numerator_nus += scalar_acquisition_operators[jsub] * f_dust.eval(self.joint.qubic.allnus[jsub])
+                    denominator_nus += scalar_acquisition_operators[jsub]
+
+                # Compute the expected resolution
+                self.fwhm_rec = np.append(self.fwhm_rec, np.sum(numerator_fwhm) / np.sum(denominator_fwhm))
+
+                # Compute the expected frequency
+                fraction = np.sum(numerator_nus) / np.sum(denominator_nus)
+                fun = lambda nu: np.abs(fraction - f_dust.eval(nu))
+                x0 = self.nus_Q[irec]
+                corrected_nu = scipy.optimize.minimize(fun, x0)
+                self.nus_Q[irec] = corrected_nu['x']
+
+            # Old way : to be remove after debugging
+            # self.fwhm_rec = np.array([])
+            # self.nus_Q = self._get_averaged_nus()
+            # for irec in range(self.params['QUBIC']['nrec']):
+            #     self.fwhm_rec = np.append(self.fwhm_rec, np.mean(self.joint.qubic.allfwhm[irec*self.fsub_out:(irec+1)*self.fsub_out]))
+            
         else:
             self.fwhm_rec = np.array([])
             for irec in range(self.params['QUBIC']['nrec']):
@@ -439,7 +488,7 @@ class PipelineFrequencyMapMaking:
     def _get_preconditionner(self):
 
         if self.params['PCG']['preconditioner']:
-            conditionner = np.ones((self.params['QUBIC']['nrec'], 12*self.params['Sky']['nside']**2, 3))            
+            conditionner = np.ones((self.params['QUBIC']['nrec'], 12*self.params['SKY']['nside']**2, 3))            
             for i in range(conditionner.shape[0]):
                 for j in range(conditionner.shape[2]):
                     conditionner[i, :, j] = 1/self.coverage_cut

spectrum/spectra.py
@@ -5,13 +5,12 @@ import yaml
 import scipy
 import pickle
 import time
-import matplotlib.pyplot as plt
 import healpy as hp
+
 #### QUBIC packages
 import qubic
 from qubic.polyacquisition import compute_freq
 from qubic import NamasterLib as nam
-from qubic.beams import BeamGaussian
 from pysimulators.interfaces.healpy import HealpixConvolutionGaussianOperator
 from pyoperators import *
 import qubic
@@ -51,7 +50,7 @@ class Spectrum:
         self.nus = self.dict_file['nus']
         self.nfreq = len(self.nus)
         self.nrec = self.params['QUBIC']['nrec']
-        self.nsub = self.params['QUBIC']['nsub']
+        self.nsub = self.params['QUBIC']['nsub_out']
         self.fsub = int(self.nsub / self.nrec)
         self.nside = self.params['SKY']['nside']
         self.nsub = int(self.fsub * self.nrec)
@@ -65,40 +64,41 @@ class Spectrum:
         self.allfwhm = self._get_allfwhm()  
         
         ############ Test
-        if self.params['QUBIC']['convolution_in'] and not self.params['QUBIC']['convolution_out']:
-            self.dict, self.dict_mono = self.get_dict()
-            self.Q = acq.QubicFullBandSystematic(self.dict, self.params['QUBIC']['nsub'], self.params['QUBIC']['nrec'], kind='UWB')
-            #joint = acq.JointAcquisitionFrequencyMapMaking(self.dict, 'UWB', self.params['QUBIC']['nrec'], self.params['QUBIC']['nsub'])
-            list_h = self.Q.H
-            h_list = np.empty(len(self.Q.allnus))
-            vec_ones = np.ones(list_h[0].shapein)
-            for freq in range(len(self.Q.allnus)):
-                h_list[freq] = np.mean(list_h[freq](vec_ones))
+        # if self.params['QUBIC']['convolution_in'] and not self.params['QUBIC']['convolution_out']:
+        #     self.dict, self.dict_mono = self.get_dict()
+        #     self.Q = acq.QubicFullBandSystematic(self.dict, self.params['QUBIC']['nsub_out'], self.params['QUBIC']['nrec'], kind='DB')
+        #     #joint = acq.JointAcquisitionFrequencyMapMaking(self.dict, 'UWB', self.params['QUBIC']['nrec'], self.params['QUBIC']['nsub'])
+        #     list_h = self.Q.H
+        #     h_list = np.empty(len(self.Q.allnus))
+        #     vec_ones = np.ones(list_h[0].shapein)
+        #     for freq in range(len(self.Q.allnus)):
+        #         h_list[freq] = np.mean(list_h[freq](vec_ones))
 
-            fwhm = [self._get_fwhm_during_MM(i) for i in self.Q.allnus]
+        #     fwhm = [self._get_fwhm_during_MM(i) for i in self.Q.allnus]
             
-            def f_beta(nu, nu_0, beta):
-                return (nu/nu_0)**beta * (np.exp(scipy.constants.h*nu*1e9/(scipy.constants.k * 20)) - 1) / (np.exp(scipy.constants.h*nu_0*1e9/(scipy.constants.k * 20)) - 1)
+        #     def f_beta(nu, nu_0, beta):
+        #         return (nu/nu_0)**beta * (np.exp(scipy.constants.h*nu*1e9/(scipy.constants.k * 20)) - 1) / (np.exp(scipy.constants.h*nu_0*1e9/(scipy.constants.k * 20)) - 1)
             
-            corrected_allfwhm = []
-            corrected_allnus = []
-            for i in range(self.nrec):
-                corrected_fwhm = (np.sum(h_list[i*self.fsub:(i+1)*self.fsub] * f_beta(self.Q.allnus[i*self.fsub:(i+1)*self.fsub], 353, 1.53) * fwhm[i*self.fsub:(i+1)*self.fsub]) / (np.sum(h_list[i*self.fsub:(i+1)*self.fsub] * f_beta(self.Q.allnus[i*self.fsub:(i+1)*self.fsub], 353, 1.53))))
-                corrected_allfwhm.append(corrected_fwhm)
-                fraction = (np.sum(h_list[i*self.fsub:(i+1)*self.fsub] * f_beta(self.Q.allnus[i*self.fsub:(i+1)*self.fsub], 353, 1.53)) / np.sum(h_list[i*self.fsub:(i+1)*self.fsub]))
-                fun = lambda nu: np.abs(fraction - f_beta(nu, 353, 1.53))
-                x0 = self.nus[i]
-                corrected_nu = scipy.optimize.minimize(fun, x0)
-                corrected_allnus.append(corrected_nu['x'])
+        #     corrected_allfwhm = []
+        #     corrected_allnus = []
+        #     for i in range(self.nrec):
+        #         corrected_fwhm = (np.sum(h_list[i*self.fsub:(i+1)*self.fsub] * f_beta(self.Q.allnus[i*self.fsub:(i+1)*self.fsub], 353, 1.53) * fwhm[i*self.fsub:(i+1)*self.fsub]) / (np.sum(h_list[i*self.fsub:(i+1)*self.fsub] * f_beta(self.Q.allnus[i*self.fsub:(i+1)*self.fsub], 353, 1.53))))
+        #         corrected_allfwhm.append(corrected_fwhm)
+        #         fraction = (np.sum(h_list[i*self.fsub:(i+1)*self.fsub] * f_beta(self.Q.allnus[i*self.fsub:(i+1)*self.fsub], 353, 1.53)) / np.sum(h_list[i*self.fsub:(i+1)*self.fsub]))
+        #         fun = lambda nu: np.abs(fraction - f_beta(nu, 353, 1.53))
+        #         x0 = self.nus[i]
+        #         corrected_nu = scipy.optimize.minimize(fun, x0)
+        #         corrected_allnus.append(corrected_nu['x'])
  
-            print('nus', self.nus)
-            print('fwhm', self.allfwhm)   
-            print('bandwith 150', qubic.compute_freq(150, Nfreq = 2))  
-            print('bandwith 220', qubic.compute_freq(220, Nfreq = 2))
-            self.nus[:self.nrec] = corrected_allnus
-            self.allfwhm[:self.nrec] = corrected_allfwhm
-            print('corrected nus', self.nus)
-            print('corrected fwhm', self.allfwhm)
+        #     print('nus', self.nus)
+        #     print('fwhm', self.allfwhm)   
+        #     print('bandwith 150', qubic.compute_freq(150, Nfreq = 2))  
+        #     print('bandwith 220', qubic.compute_freq(220, Nfreq = 2))
+        #     self.nus[:self.nrec] = corrected_allnus
+        #     self.allfwhm[:self.nrec] = corrected_allfwhm
+        #     print('corrected nus', self.nus)
+        #     print('corrected fwhm', self.allfwhm)
+        
               
         self.kernels150 = np.sqrt(self.fwhm150[0]**2 - self.fwhm150[-1]**2)
         self.kernels220 = np.sqrt(self.fwhm220[0]**2 - self.fwhm220[-1]**2)
@@ -141,7 +141,7 @@ class Spectrum:
 
         args = {'npointings':self.params['QUBIC']['npointings'], 
                 'nf_recon':self.params['QUBIC']['nrec'], 
-                'nf_sub':self.params['QUBIC']['nsub'], 
+                'nf_sub':self.params['QUBIC']['nsub_out'], 
                 'nside':self.params['SKY']['nside'], 
                 'MultiBand':True, 
                 'period':1, 