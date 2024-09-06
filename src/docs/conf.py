dc456b6de4db6afc6866506ea3a2d3bdd713d30a
TomLaclavere <laclavere@apc.in2p3.fr>
2024-09-06 15:04:18 +0200

    Add githubpages extension

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -25,6 +25,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
     'sphinx_autodoc_typehints',
     "numpydoc",
     'sphinx.ext.mathjax',
