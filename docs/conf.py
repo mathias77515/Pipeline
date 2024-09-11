ee94884c483dee19ba9a6d0c85d6f7a8bfafc047
TomLaclavere <laclavere@apc.in2p3.fr>
2024-09-11 11:17:07 +0200

    Add documentation repository

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
