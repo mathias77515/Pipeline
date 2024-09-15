0f6622c385967aa6ec1e5688a5208a9edebbaf91
TomLaclavere <tlaclave@cca016.in2p3.fr>
2024-09-15 11:41:55 +0200

    Study Nsub_in

--- src/lib/Qacquisition.py
+++ src/lib/Qacquisition.py
@@ -1817,7 +1817,7 @@ class QubicMultiAcquisitions:
 
     """
 
-    def __init__(self, dictionary, nsub, nrec=1, comps=[], H=None, nu_co=None):
+    def __init__(self, dictionary, nsub, nrec=1, comps=[], H=None, sampling=None):
 
         ### Define class arguments
         self.dict = dictionary
@@ -1859,7 +1859,10 @@ class QubicMultiAcquisitions:
         self.multiinstrument = instr.QubicMultibandInstrumentTrapezoidalIntegration(
             self.dict
         )
-        self.sampling = qubic.get_pointing(self.dict)
+        if sampling == None:
+            self.sampling = qubic.get_pointing(self.dict)
+        else:
+            self.sampling = sampling
         self.scene = qubic.QubicScene(self.dict)
         self.npix = 12 * self.scene.nside**2
 
